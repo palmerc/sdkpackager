9203d509d0ca06b7bfc1e2e96df454eb8a4d43e5
Cameron Lowell Palmer <cameron@promon.no>
2024-02-16 14:24:02 +0100

    Add ability to package more sdks like AppleTVOS

--- package.py
+++ package.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import pathlib
+import plistlib
 import subprocess
 import tarfile
 
@@ -75,10 +76,28 @@ def sdk_files(sdk):
     return [x for x in sdk_path(sdk).rglob('*')]
 
 
+def xcode_version(path):
+    with open(path / 'version.plist', 'rb') as f:
+        version_plist = plistlib.load(f)
+        return version_plist['CFBundleShortVersionString']
+
+
 def package(sdks):
-    with tarfile.open(f'AppleSDK.tar.xz', mode='w:xz') as t:
+    version = xcode_version(xcode_path().parent)
+    print(f'Xcode {version}')
+
+    sdks.sort()
+    print(f'Requested: {", ".join(sdks)}')
+    available_sdks = [p.casefold() for p in platforms()]
+    missing_sdks = set(sdks) - set(available_sdks)
+    sdks = list(set(sdks) - missing_sdks)
+    sdks.sort()
+    print(f'Available: {", ".join(sdks)}')
+    
+    output_file = f'AppleSDK_Xcode{version}.tgz'
+    with tarfile.open(output_file, mode='w:gz') as t:
         for sdk in sdks:
-            print(f'Packaging - {sdk_name(sdk)}')
+            print(f'Packaging: {sdk_name(sdk)}')
             sdk_file_paths = [x for x in sdk_files(sdk) if x.is_file()]
             for sdk_file_path in sdk_file_paths:
                 archive_name = sdk_file_path.relative_to(sdk_path(sdk)).as_posix()
@@ -87,26 +106,28 @@ def package(sdks):
                     info = t.gettarinfo(arcname=name, fileobj=f)
                     t.addfile(tarinfo=info, fileobj=f)
 
-        print(f'Packaging - XcodeDefault.xctoolchain')
+        print(f'Packaging: XcodeDefault.xctoolchain')
         xc_toolchain_paths = [x for x in xcode_toolchain_files() if x.is_file()]
         for xc_toolchain_path in xc_toolchain_paths:
             archive_name = xc_toolchain_path.relative_to(xcode_toolchain_path()).as_posix()
             info = tarfile.TarInfo(name=f'XcodeDefault.xctoolchain/{archive_name}')
             with open(xc_toolchain_path, 'rb') as f:
                 t.addfile(info, f)
-
+    print(f'Wrote: {output_file}')
 
 def main():
+    platforms = ['appletvos', 'appletvsimulator', 'driverkit', 'macosx', 'watchos', 'watchsimulator', 'xros', 'xrsimulator', 'iphoneos', 'iphonesimulator']
+
     parser = argparse.ArgumentParser(description='Package the Xcode SDKs')
-    parser.add_argument('--sdk', default='all', choices=['iphoneos', 'iphonesimulator', 'macosx', 'all'], required=False,
+    parser.add_argument('--sdk', nargs='+', default='all', choices=platforms + ['all'], required=False,
                         help='Specify the SDK to package')
+
     args = parser.parse_args()
-    if args.sdk == 'all':
-        sdks = ['iphoneos', 'iphonesimulator', 'macosx']
+    if not args.sdk or args.sdk == 'all':
+        sdks = platforms
     else:
         sdks = [args.sdk]
 
-    print(f'Package {",".join(sdks)}')
     package(sdks)
 
 
